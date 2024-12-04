a3995c484683b2cdab58c3f3652e08a2279e1efd
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-04 12:55:20 +0400

    leetcode 279

---
+++ src/main/java/example_five/PerfectSquares.java
@@ -0,0 +1,25 @@
+package example_five;
+
+import java.util.Arrays;
+
+public class PerfectSquares {
+    public static void main(String[] args) {
+        int n = 13;
+        int result = perfectSquares(n);
+        System.out.println(result);
+    }
+
+    private static int perfectSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
