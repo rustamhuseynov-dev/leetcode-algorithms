aa8766b365a759cfe4e9715f474edccb10a33004
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2026-01-05 10:35:35 +0400

    Algorithms Task#1

---
+++ src/main/java/algoritms/FindTheIndexOfTheFirstOccurrenceString.java
@@ -0,0 +1,11 @@
+package algoritms;
+
+public class FindTheIndexOfTheFirstOccurrenceString {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+
+        int value = haystack.indexOf(needle);
+        System.out.println(value);
+    }
+}
