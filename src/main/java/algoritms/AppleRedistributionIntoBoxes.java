174eb44e7629a2bc6023409738e78d320286dd0d
rustamhuseynov-dev <rustem.huseynov.2016@gmail.com>
2026-01-05 10:33:47 +0400

    Algorithms Task#1

---
+++ src/main/java/algoritms/AppleRedistributionIntoBoxes.java
@@ -0,0 +1,36 @@
+package algoritms;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class AppleRedistributionIntoBoxes {
+    public static void main(String[] args) {
+        int[] apple = {1,8,3,3,5};
+        int[] capacity = {3,9,5,1,9};
+        int i = minimumBoxes(apple, capacity);
+        System.out.println(i);
+    }
+    public static int minimumBoxes(int[] apple, int[] capacity) {
+        int apples = appleSum(apple);
+        int count = 0;
+        Integer[] boxedCapacity = Arrays.stream(capacity)
+                .boxed()
+                .toArray(Integer[]::new);
+        Arrays.sort(boxedCapacity, Collections.reverseOrder());
+        for(int a = 0; a < boxedCapacity.length; a++){
+            if(apples > 0){
+                apples = apples - boxedCapacity[a];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int appleSum(int[] apple){
+        int sum = 0;
+        for (int j : apple) {
+            sum += j;
+        }
+        return sum;
+    }
+}
